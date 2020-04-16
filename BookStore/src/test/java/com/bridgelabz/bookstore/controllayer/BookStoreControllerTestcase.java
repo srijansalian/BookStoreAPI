e0ef0ef529a50cccfd0a733efe53c6e121a33acb
srijansalian <srijansalian@gmail.com>
2020-04-16 11:59:15 +0530

    [Srijan].Created folders for the control and service layer test case

---
+++ BookStore/src/test/java/com/bridgelabz/bookstore/controllayer/BookStoreControllerTestcase.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.bookstore.controllayer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookStoreControllerTestcase {
+
+	@Test
+	void testAddBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBooks() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BookStore/src/test/java/com/bridgelabz/bookstore/servicelayer/BookServiceImplTestcase.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.bookstore.servicelayer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookServiceImplTestcase {
+
+	@Test
+	void testAddBooks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBookInfo() {
+		fail("Not yet implemented");
+	}
+
+}
