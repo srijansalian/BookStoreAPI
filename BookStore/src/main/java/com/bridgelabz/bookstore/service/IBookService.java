8a4de1bc765ea44a319dc14917c375218c500d77
srijansalian <srijansalian@gmail.com>
2020-04-16 14:15:39 +0530

    [Srijan].Updated Hibernate repository into the JPA Repositorys

--- BookStore/src/main/java/com/bridgelabz/bookstore/service/IBookService.java
+++ BookStore/src/main/java/com/bridgelabz/bookstore/service/IBookService.java
@@ -8,9 +8,6 @@ import com.bridgelabz.bookstore.entity.BookInformation;
 public interface IBookService {
 	//public ResponseEntity<BookResponse> addBooks(BookDto information);
 	boolean addBooks(BookDto information);
-
-	
-	
 	List<BookInformation> getBookInfo();
 
 }
